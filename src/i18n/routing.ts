24611cb7ff2714984af5d049d9440d3c0f841851
Glauk Thaqi <glaukthaqi15@gmail.com>
2025-11-12 21:31:47 +0100

    default language

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,7 @@
-import {defineRouting} from 'next-intl/routing';
- 
+import { defineRouting } from 'next-intl/routing';
+
 export const routing = defineRouting({
   locales: ['sq', 'en'],
- 
-  defaultLocale: 'sq'
+
+  defaultLocale: 'en',
 });
